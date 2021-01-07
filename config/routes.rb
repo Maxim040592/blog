8516cd16959399228e3b8cf24b4ffc4b8d3d2479
Maxim040592 <maxim_007_92@mail.ru>
2021-01-07 22:08:15 +0500

    add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
 get '/' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root to: "home#index"
+
+
 get 'terms' => 'pages#terms'
 get 'about' => 'pages#about'
 resource :contacts, only: [:new, :create], path_names: { :new => ''}  # маршруты только на отображение и отрпавку

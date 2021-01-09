9a0c30947887bb458009da7aafd411989ba8f079
Maxim040592 <maxim_007_92@mail.ru>
2021-01-09 22:58:06 +0500

    Add method for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ApplicationRecord
    validates :text, presence: true
    has_many :comments #из модели сможем обращаться к контроллеру comments т.е связали сущности
    #belongs_to :user # связывание сущностей users и arcticle
+
+   def subject
+     title #заголовок статьи
+   end
 end

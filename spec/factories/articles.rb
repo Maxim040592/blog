b514e6dc2c0a08ea99c789498d691d904f1734ea
Maxim040592 <maxim_007_92@mail.ru>
2021-01-09 23:34:48 +0500

    Create Factory article_with_comments

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :article do
     title "Article title" #просто текст чтобы сущность не была пустой
     text "Article text"   #просто текст чтобы сущность не была пустой
+
+    #создаем фабрику с названием article_with_comments
+    # для создания статьи с несколькими комментарими
+    factory :article_with_comments do
+      #после создания article
+      after :create do |article, evaluator|
+        #создаем список из 3-х комментов
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end

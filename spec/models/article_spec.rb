cd7e5b6bf7a4e9be60e297134e2a45806dbe0521
Maxim040592 <maxim_007_92@mail.ru>
2021-01-09 20:59:38 +0500

    update article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 
 describe Article do 
 
-  it { should have_many :comments } 
-  #т.е сущность articles должна емть много комментариев 
-  #т.е в модели Article должен быть прописан код has_many :comments
- 
+  describe "Validations" do 
+    it { should validate_presence_of :title } # В модели Contact должна присутвовать валидация поля :email
+    it { should validate_presence_of :text } 
+ end
+
+  describe "associations" do
+    it { should have_many :comments } 
+    #т.е сущность articles должна емть много комментариев 
+    #т.е в модели Article должен быть прописан код has_many :comments
+  end
  
 end

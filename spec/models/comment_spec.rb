5c6e25f22dda07d1e66a36b6eb6e5954717e4ed1
Maxim040592 <maxim_007_92@mail.ru>
2021-01-09 20:52:42 +0500

    Add comment model spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+require 'rails_helper'
+
+describe Comment do 
+
+  it { should belong_to :article } 
+   #т.е сущность comment должна емть одну статью 
+  #т.е в модели Comment должен быть прописан код belong_to :article
+ 
+end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 describe Contact do 
 
-  it { should validate_presence_of :email } 
+  it { should validate_presence_of :email } # В модели Contact должна присутвовать валидация поля :email
   it { should validate_presence_of :message } 
  
  

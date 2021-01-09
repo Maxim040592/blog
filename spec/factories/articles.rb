222e05dc813329f4bd8d3afb19382831029e6633
Maxim040592 <maxim_007_92@mail.ru>
2021-01-09 22:56:57 +0500

    Add factory for spec

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do 
+  factory :article do
+    title "Article title" #просто текст чтобы сущность не была пустой
+    text "Article text"   #просто текст чтобы сущность не была пустой
+  end
+end

c4e58b14432647c74f7e276374a0d8401186595d
Maxim040592 <maxim_007_92@mail.ru>
2021-01-10 15:55:09 +0500

    Add validate max and min length field

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
    validates :title, presence: true
    validates :text, presence: true
+   validates_length_of :title, maximum: 140
+   validates_length_of :text, maximum: 4000
    has_many :comments #из модели сможем обращаться к контроллеру comments т.е связали сущности
    #belongs_to :user # связывание сущностей users и arcticle
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  validates_length_of :body, minimum: 5
   belongs_to :article # связывание сущностей coment и arcticle
 end
